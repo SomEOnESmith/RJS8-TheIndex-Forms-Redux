c16a11c6d1ad50d9384ae0aceab6dd267b1f32f0
Huusain_AlHaddad <h.ali.alhaddad@hotmail.com>
2019-09-26 15:16:30 +0300

    sarah, hussain

--- src/BookForm.js
+++ src/BookForm.js
@@ -7,13 +7,13 @@ import * as actionCreators from "./store/actions/index";
 class BookForm extends Component {
   state = {
     title: "",
-    color: "",
+    color: "red",
     authors: [this.props.author.id]
   };
 
-  // componentWillUnmount() {
-  //   if (this.props.errors.length) this.props.resetErrors();
-  // }
+  componentWillUnmount() {
+    if (this.props.errors.length) this.props.resetErrors();
+  }
 
   submitBook = event => {
     event.preventDefault();
@@ -26,9 +26,20 @@ class BookForm extends Component {
     });
   };
   render() {
+    const errors = this.props.errors;
+    let colors = ["red", "blue", "black", "white", "yellow", "green"].map(
+      color => <option>{color}</option>
+    );
     return (
       <div className="mt-5 p-2">
         <form onSubmit={this.submitBook}>
+          {!!errors.length && (
+            <div className="alert alert-danger" role="alert">
+              {errors.map(error => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          )}
           <div className="input-group mb-3">
             <div className="input-group-prepend">
               <span className="input-group-text">Title</span>
@@ -44,24 +55,14 @@ class BookForm extends Component {
             <div className="input-group-prepend">
               <span className="input-group-text">Book Color</span>
             </div>
-            <input
-              type="text"
-              className="form-control"
+            <select
               name="color"
-              onChange={this.handleChange}
-            />
-          </div>
-          {/* <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text">Authors</span>
-            </div>
-            <input
-              type="text"
               className="form-control"
-              name="authors"
               onChange={this.handleChange}
-            />
-          </div> */}
+            >
+              {colors}
+            </select>
+          </div>
           <input type="submit" />
         </form>
       </div>
@@ -71,15 +72,15 @@ class BookForm extends Component {
 
 const mapStateToProps = state => {
   return {
-    // errors: state.rootErrors.errors
+    errors: state.rootErrors.errors
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     postBook: (newBook, closeModal) =>
-      dispatch(actionCreators.postBook(newBook, closeModal))
-    // resetErrors: () => dispatch(actionCreators.resetErrors())
+      dispatch(actionCreators.postBook(newBook, closeModal)),
+    resetErrors: () => dispatch(actionCreators.resetErrors())
   };
 };
 
@@ -87,11 +88,3 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(BookForm);
-
-// {!!errors.length && (
-//   <div className="alert alert-danger" role="alert">
-//     {errors.map(error => (
-//       <p key={error}>{error}</p>
-//     ))}
-//   </div>
-// )}
